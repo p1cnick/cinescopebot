d5a688d3f1ad6d4d13f6cc47ac3f300f5329584f
p1cnick <105843220+p1cnick@users.noreply.github.com>
2022-07-27 12:22:15 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -180,7 +180,7 @@ def get_size(size):
     size = float(size)
     i = 0
     while size >= 1024.0 and i < len(units):
-        i += 1
+        i += 3
         size /= 1024.0
     return "%.2f %s" % (size, units[i])
 
